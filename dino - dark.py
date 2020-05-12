69b33130cc9ec5c92f8588dc5e0faca7f09c4890
heykush <57096457+heykush@users.noreply.github.com>
2020-05-12 17:50:02 +0530

    Add files via upload
    Here's both file

---
+++ dino - dark.py
@@ -0,0 +1,45 @@
+import pyautogui
+from PIL import Image, ImageGrab
+from numpy import asarray 
+import PIL.ImageOps  
+import time
+def hit(key):
+    pyautogui.press(key)
+    return
+def isCollide(data):
+   #Draw  the rectangle for cactus
+    for i in range(158, 275):
+        for j in range(420, 450):
+            if data[i, j] <= 84 :       
+                hit("up")
+                return
+     # Draw the rectangle for birds
+    for i in range(165, 200):
+        for j in range(305, 399):
+            if  data[i, j] <= 84:
+                hit("down")
+                return
+    return
+if __name__ == "__main__":
+    time.sleep(2)
+    print("Hey.. Dino game about to start in 3 seconds")
+    # hit('up') 
+    while True:
+        image = ImageGrab.grab().convert('L')
+        inverted_image = PIL.ImageOps.invert(image)
+        data = inverted_image.load()
+        isCollide(data)       
+        
+        # #     # Draw the rectangle for birds
+        # for i in range(170, 200):
+        #     for j in range(305, 395):
+        #         data[i, j] = 0
+            
+        #     # Draw the rectangle for Catcus
+        # for i in range(158, 275):
+        #     for j in range(420, 450):   
+        #         data[i, j] = 82
+
+        # inverted_image.show()
+        # break
+            
---
+++ dino - light.py
@@ -0,0 +1,45 @@
+import pyautogui
+from PIL import Image, ImageGrab
+from numpy import asarray 
+import PIL.ImageOps  
+import time
+def hit(key):
+    pyautogui.press(key)
+    return
+def isCollide(data):
+   #Draw  the rectangle for cactus
+    for i in range(190, 238):
+        for j in range(395, 460):  
+            if data[i, j] < 100 :       
+                hit("up")
+                return
+     # Draw the rectangle for birds
+    for i in range(170, 210):
+        for j in range(270, 395):
+            if  data[i, j] < 100:
+                hit("down")
+                return
+    return
+if __name__ == "__main__":
+    # time.sleep(2)
+    print("Hey.. Dino game about to start in 3 seconds")
+    # hit('up') 
+    while True:
+        image = ImageGrab.grab().convert('L')
+        # inverted_image = PIL.ImageOps.invert(image)
+        data =image.load()
+        isCollide(data)       
+        
+        # # #Draw the rectangle for birds
+        # for i in range(170, 210):
+        #     for j in range(270, 395):
+        #         data[i, j] = 82
+            
+        #     # Draw the rectangle for Catcus
+        # for i in range(190, 238):
+        #     for j in range(395, 460):   
+        #         data[i, j] = 0
+
+        # image.show()
+        # break
+            
